99c74ed410014f1fa62d74173d7ba8ead226463f
Frank Riley <fhriley@gmail.com>
2017-03-16 21:44:07 -0700

    work around bug in ipmitool

--- bin/fans.py
+++ bin/fans.py
@@ -4,6 +4,7 @@ import re
 from time import sleep
 from datetime import datetime
 import argparse
+import json
 from pprint import pprint
 
 IMPITOOL_BIN = '/usr/local/bin/ipmitool'
@@ -18,8 +19,8 @@ def get_duty_cycle(zone):
 def set_duty_cycle(zone, value):
     subprocess.check_output((DUTY_CYCLE_CMD % (1, zone) + ' %d' % value).split())
 
-def get_duty_cycles():
-    return get_duty_cycle(0), get_duty_cycle(1)
+def get_duty_cycles(zones):
+    return [get_duty_cycle(zone) for zone in zones]
 
 def get_mode():
     mode = int(subprocess.check_output((MODE_CMD % 0).split()), 16)
@@ -29,61 +30,56 @@ def set_mode(mode):
     mode = dict((val, key) for key, val in MODES.iteritems())[mode]
     subprocess.check_output((MODE_CMD % 1 + ' %d' % mode).split())
 
-def get_speeds():
+def get_speeds(zones):
     sdr_re = re.compile(r'(FAN\S+)\s*\|.*\|\s*(\S+)\s*\|.*\|\s*(\d+)\s*RPM\s*\n')
-    saved_dc = get_duty_cycles()
-    try:
-        # this command cause duty cycle reads after this to be wrong
-        sdr = subprocess.check_output(FAN_SPEEDS_CMD.split())
-        return dict((name, {'speed': int(speed), 'status': status})
+    # warning: this command cause duty cycle reads after this to be wrong
+    sdr = subprocess.check_output(FAN_SPEEDS_CMD.split())
+    return dict((name, {'speed': int(speed), 'status': status})
                     for name, status, speed in sdr_re.findall(sdr))
-    finally:
-        try: set_duty_cycle(0, saved_dc[0])
-        except: pass
-        try: set_duty_cycle(1, saved_dc[1])
-        except: pass
 
-def print_header(fans):
+def print_header(zones, fans):
     rpm = 'Curr_RPM' + '_' * len(fans) * 9
-    print('{:25}___Duty%___ {}'.format('', rpm))
+    duty = '_' * (3 * (len(zones) - 1)) + 'Duty%' + '_' * (3 * (len(zones) - 1))
+    print('{:25}{} {}'.format('', duty, rpm))
+
+    zones = ' '.join(['{:>5}'.format('Zone%d' % zone) for zone in zones])
     fan_headers = ' '.join(['{%s:10}' % key for key in sorted(fans.keys())])
-    print(("{cond:15} {mode:8} {duty0:>5} {duty1:>5} %s" % fan_headers).format(
-        cond='', mode="MODE", duty0="Zone0", duty1="Zone1",
+    print(("{cond:15} {mode:8} {zones} %s" % fan_headers).format(
+        cond='', mode="MODE", zones = zones,
         **dict((key, key) for key in fans.keys())))
 
 def print_line(cond, mode, duties, speeds):
     fans = ' '.join(['{:10}'.format('%s(%s)' % (speeds[key]['speed'], speeds[key]['status']))
                      for key in sorted(speeds.keys())])
-    print('{cond:15} {mode:8} {duty0:>5} {duty1:>5} {fans}'.format(
-        cond=cond, mode=mode, duty0=duties[0], duty1=duties[1], fans=fans))
+    duties = ' '.join(['{:>5}'.format(dc) for dc in duties])
+    print('{cond:15} {mode:8} {duties} {fans}'.format(
+        cond=cond, mode=mode, duties=duties, fans=fans))
 
 def test_mode(args):
     print(datetime.now().strftime('%A, %b %d, %H:%M:%S'))
+    saved_dc = get_duty_cycles(args.zones)
     saved_mode = get_mode()
-    saved_dc = get_duty_cycles()
-    speeds = get_speeds()
-    print_header(speeds)
-    print_line('Starting state', saved_mode, saved_dc, speeds)
     try:
+        speeds = get_speeds(args.zones)
+        print_header(args.zones, speeds)
+        print_line('Starting state', saved_mode, saved_dc, speeds)
+
         set_mode('Full')
-        sleep(1)
 
         for duty_cycle in range(args.dc_max, args.dc_min-10, -10):
-            set_duty_cycle(0, duty_cycle)
-            set_duty_cycle(1, duty_cycle)
+            for zone in args.zones:
+                set_duty_cycle(zone, duty_cycle)
             sleep(5)
-            print_line('Duty cycle %d' % duty_cycle, saved_mode, get_duty_cycles(), get_speeds())
+            print_line('Duty cycle %d' % duty_cycle, saved_mode,
+                       get_duty_cycles(args.zones), get_speeds(args.zones))
     except KeyboardInterrupt:
         pass
     finally:
         try: set_mode(saved_mode)
         except: pass
-        try: set_duty_cycle(0, saved_dc[0])
-        except: pass
-        try: set_duty_cycle(1, saved_dc[1])
-        except: pass
-        sleep(5)
-    print_line('Ending state', get_mode(), get_duty_cycles(), get_speeds())
+        for zone in args.zones:
+            try: set_duty_cycle(zone, saved_dc[zone])
+            except: pass
 
 def log_mode(args):
     pass
@@ -91,11 +87,26 @@ def log_mode(args):
 def pid_mode(args):
     pass
 
+def show_mode(args):
+    saved_dc = get_duty_cycles(args.zones)
+    data = {
+        'mode': get_mode(),
+        'duty_cycle': saved_dc,
+    }
+    try:
+        data.update(get_speeds(args.zones))
+    finally:
+        for zone in args.zones:
+            try: set_duty_cycle(zone, saved_dc[zone])
+            except: pass
+    print(json.dumps(data, indent=4, sort_keys=True))
+
 if __name__ == '__main__':
     modes = {
         'test': test_mode,
         'log': log_mode,
         'pid': pid_mode,
+        'show': show_mode,
     }
     parser = argparse.ArgumentParser(description='Fan control')
     parser.add_argument('mode', choices=sorted(modes.keys()),
@@ -110,5 +121,6 @@ if __name__ == '__main__':
 
     if args.dc_min > args.dc_max:
         parser.error('duty cycle min must be <= duty cycle max')
+    args.zones = range(0, args.zones)
 
     modes[args.mode](args)
